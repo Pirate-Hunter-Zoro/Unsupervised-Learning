9dd8f4e7135c4f82461f53d64d09eb05af241352
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-11-29 17:37:33 -0600

    kmeans implemented

--- src/clustering.py
+++ src/clustering.py
@@ -1,17 +1,4 @@
 import numpy as np
-import random
-
-def euclidean_distance(a: np.array, b: np.array) -> float:
-    """Euclidean distance helper
-
-    Args:
-        a (np.array): first point
-        b (np.array): second point
-
-    Returns:
-        float: resulting distance
-    """
-    return np.linalg.norm(a-b)
 
 class KMeans:
     
@@ -26,6 +13,7 @@ class KMeans:
         self.n_clusters = n_clusters
         self.max_iters = max_iters
         self.tol = tol
+        self.centers = None
         
     def fit(self, X: np.array):
         """Method to run the kmeans algorithm to estimate the centroids
@@ -33,9 +21,41 @@ class KMeans:
         Args:
             X (np.array): Points generated from various gaussian distributions (whose means and standard deviations we do not know here)
         """
-        # TODO - keep in numpy
-        centers = np.array(random.sample(X.tolist(), self.n_clusters))
-        
+        centers = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)] # Shape (K, Features), whereas X has shape (N, Features)
+        X_reshaped = X.reshape(X.shape[0], 1, X.shape[1])
+        centers = centers.reshape(1, centers.shape[0], centers.shape[1])
+        # Now when we subtract, numpy will automatically broadcast and we'll get shape (N, K, Features), where each point will have its distance vector from all center points
         for _ in range(self.max_iters):
             # Assign each point to its closest centroid
-            centroid_distances = [()]
+            diff_vectors = X_reshaped - centers # (N, K, Features)
+            center_distances = np.linalg.norm(diff_vectors, axis=2) # (N, K)
+            # Each point is assigned the center it is closest to
+            assignments = np.argmin(center_distances, axis=1) # (N, 1)
+            # Now each class assignment has a different centroid
+            new_centers = centers.copy()
+            for center_class in range(self.n_clusters):
+                points = X[assignments==center_class]
+                if points.shape[0] == 0:
+                    continue
+                class_mean = np.mean(points, axis=0)
+                new_centers[0][center_class] = class_mean
+            if np.linalg.norm(new_centers - centers) < self.tol:
+                break
+            else:
+                centers = new_centers
+        self.centers = centers.reshape(centers.shape[1], centers.shape[2]) # (K, Features)
+        
+    def predict(self, X: np.array) -> np.array:
+        """Prediction method to use the class instance's centers to predict the class of each observation in X
+
+        Args:
+            X (np.array): Input observations
+
+        Returns:
+            np.array: Output predicted classes
+        """
+        X_reshaped = X.reshape(X.shape[0], 1, X.shape[1])
+        diff_vectors = X_reshaped - self.centers.reshape(1, self.centers.shape[0], self.centers.shape[1]) # (N, K, Features)
+        center_distances = np.linalg.norm(diff_vectors, axis=2) # (N, K)
+        # Each point is assigned the center it is closest to
+        return np.argmin(center_distances, axis=1) # (N, 1)
